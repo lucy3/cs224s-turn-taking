c5830206cdd4a07c655214ef48b90f8f2758820b
Lucy Li <lucy3@corn10.stanford.edu>
2017-05-31 15:43:44 -0700

    things look good for 5 dialogues

--- turn_labels.py
+++ turn_labels.py
@@ -21,7 +21,9 @@ from difflib import SequenceMatcher
 
 TIME_PATH = '/afs/ir/data/linguistic-data/Switchboard/SWBD-MSState-Transcripts/swb_ms98_transcriptions'
 ANNOTATED_PATH = './swda/'
-FILLERS = ['you see', 'you know', 'anyways', 'actually', 'yeah', 'okay', 'anyway', 'now', 'ok', 'like', 'well', 'say', 'so', 'see', 'uh', 'um', 'oh', 'huh', 'uhhuh', 'huhuh', 'right', 'sure', 'yes', 'yep', 'hum', 'umhum', 'hm', 'thats right', 'thats true', 'true', 'wow', 'nice', 'cool']
+ONE_FILLERS = ['anyways', 'actually', 'yeah', 'okay', 'anyway', 'now', 'ok', 'like', 'well', 'say', 'so', 'see', 'uh', 'um', 'oh', 'huh', 'uhhuh', 'huhuh', 'right', 'sure', 'yes', 'yep', 'hum', 'umhum', 'hm', 'true', 'wow', 'nice', 'cool']
+TWO_FILLERS = ['you see', 'you know', 'thats right', 'thats true']
+OUTPUT = './labels'
 
 def pair_files():
     """
@@ -102,7 +104,7 @@ def get_turns(ann_file):
             else:
                 b_ended = False
         clean_test = utt.text.replace('{D', '').replace('{F','').replace('{E', '').replace('{C','').replace('{A','')
-        clean_test = re.sub('<[_a-zA-Z\s]*>', '', clean_test)
+        clean_test = re.sub('<[_a-zA-Z/\s]*>', '', clean_test)
         clean_test = clean_test.lower().translate(None, string.punctuation)
         clean_test = re.sub(' +',' ', clean_test).strip()
         if utt.caller == 'A':
@@ -152,7 +154,7 @@ def get_times(A_file, B_file):
                 B_words.append(word) 
     return (A_words, A_times, B_words, B_times)
 
-def label(turns, backchannel, words, times):
+def label(turns, backchannel, words, times, out, ID, speaker):
     """
     TODO everything here
     TODO add output file as an input parameter
@@ -160,59 +162,61 @@ def label(turns, backchannel, words, times):
         - turns: list of SENTENCES for each turn
         - backchannel: same length as turns, list of booleans
         - words: list of WORDS 
-        - times: same length as words, list of end-start tuples
+        - times: same length as words, list of start-end tuples
     """
+    turn_indices = set()
+    pointer = 0
+    turn_indices.add(-1)
+    reinf_indices = set()
+    for i in range(len(turns)): 
+        turn = turns[i]
+        if backchannel[i] == True:
+            reinf_indices.update(range(pointer, pointer + len(turn.split())))
+        pointer += len(turn.split())
+        turn_indices.add(pointer-1)
     tok_turns = ' '.join(turns).split() # list of WORDS
-    """
-    # TODO: 
-    should get all indices that are at the end of a turn in tok_turns
-    To do this I would count the lengths of each turn in turns that is split
-    so the first index would be the length of the first tokenized turn, 
-    the second index would be the added length of the first and second, 
-    the third index would be the added length of the first three tokenized
-    turns... and so on. 
-    Similarily, one should get indices in tok_turns that are associated
-    with backchannels. 
-    """
     seq = SequenceMatcher()
     seq.set_seqs(tok_turns, words) # take in two lists of words for each transcription
-    # TODO: remove this print statement. It is here for now so you can see how
-    # the alignment is operating. 
+    time_to_turns = {i:None for i in range(len(words))}
+    # Only align same-length replacements or equivalent components 
     for tag, i1, i2, j1, j2 in seq.get_opcodes():
-        print ("%7s a[%d:%d] (%s) b[%d:%d] (%s)" % (tag, i1, i2, tok_turns[i1:i2], j1, j2, words[j1:j2]))
-    '''
-    tag - edit operation, which is replace, insert, delete, equal
-    i1, i2 - indices of tok_turns
-    j1, j2 - corresponding indices of words
-    Non problematic edit operations:
-        replacing slices of the same length. treat those indices as equal. 
-        equal. treat those indices as equal. 
-    Other edit operations are problematic.
-    Somehow store a mapping between indices in words and indices in tok_turns!
-    If the indices are problematic ones, maybe store the mapping as idx -> None.
-
-    For every filler in words: 
-        BE CAREFUL OF FILLERS THAT ARE TWO WORDS LONG. In those cases you
-        want to remember the previous word. Check for 'you see' before
-        you check for 'see', etc. The start and end times of those will 
-        be the start of the first word and the end of the second word. 
-        BE CAREFUL OF IF-STATEMENT ORDERING. We want to make sure
-        we label reinforcements first since they are often single words
-        that would be labeled as starts/ends if we did a different ordering. 
-        If the filler is in a problematic index in the variable words, 
-            ignore it and continue.
-        If the filler is mapped to an index that is associated
-        with backchanneling, label it as reinforcement. 
-        If the filler's mapped index in tok_turns is after an index that
-            is the end of turn, it is labeled as seeking a turn. 
-        If a filler is mapped to an index in tok_turns that is at
-            the end of turn, it is labeled as end of a turn. 
-        Otherwise, it is labeled as a continuation. 
-    Don't forget to output these labels into the output file so we
-    don't have to rerun label collection when we're trying to collect
-    audio clips... 
-    Print often!! scream!!
-    '''
+        if (tag == 'replace' and i2-i1 == j2-j1) or tag == 'equal':
+            match = i1
+            for j in range(j1, j2):
+                time_to_turns[j] = match
+                match += 1
+    for i in range(len(words)):
+        w = words[i]
+        if not time_to_turns[i]:
+            continue
+        start, end = times[i]
+        label = None
+        # first, two word fillers: 
+        if ((w == 'know' or w == 'see') and i - 1 > 0 and words[i-1] == 'you') or\
+            ((w == 'true' or w == 'right') and i - 1 > 0 and words[i-1] == 'thats'):
+                start, _ = times[i-1]
+                if time_to_turns[i-1] not in turn_indices: # this two-word phrase is part of the same turn
+                    if time_to_turns[i] in reinf_indices: 
+                        label = 'reinforce'
+                    elif time_to_turns[i-1]-1 in turn_indices:
+                        label = 'seek'
+                    elif time_to_turns[i] in turn_indices:
+                        label = 'end'
+                    else:
+                        label = 'continue'
+                    out.write(ID + '_' + speaker + '_' + str(start) + '_' + \
+                            str(end) + '_' + words[i-1] + w + '\t' + label + '\n')
+        elif w in ONE_FILLERS: # then, one word fillers
+            if time_to_turns[i] in reinf_indices:
+                label = 'reinforce'
+            elif time_to_turns[i]-1 in turn_indices:
+                label = 'seek'
+            elif time_to_turns[i] in turn_indices:
+                label = 'end'
+            else:
+                label = 'continue'
+            out.write(ID + '_' + speaker + '_' + str(start) + '_' + \
+                    str(end) + '_' + w + '\t' + label + '\n')
 
 def align(pairs):
     """
@@ -221,13 +225,7 @@ def align(pairs):
     Note that a_turns is a list of strings for each turn, while
     A_words is a list of words. 
     """
-    # TODO: open an output file to write to
-    # That tab delimited output file should have one column that is well-designed
-    # ID for a filler. I suggest dialogueID_starttime_endtime_lexical,
-    # e.g. 4726_1.0298_1.6098_uh 
-    # the second column would be the label for that filler. 
-    # This output file would later be used to extract clips. 
-    i = 0
+    out = open(OUTPUT, 'w')
     for ID in pairs:
         p = pairs[ID]
         A_time_file = None
@@ -241,11 +239,8 @@ def align(pairs):
             B_time_file = p[1]
         a_turns, b_turns, a_backchannel, b_backchannel = get_turns(ann_file)
         A_words, A_times, B_words, B_times = get_times(A_time_file, B_time_file)
-        label(a_turns, a_backchannel, A_words, A_times)
-        label(b_turns, b_backchannel, B_words, B_times)
-        i += 1 # TODO: remove this break when done testing on small subset of data.
-        if i > 5:
-            break
+        label(a_turns, a_backchannel, A_words, A_times, out, ID, 'A')
+        label(b_turns, b_backchannel, B_words, B_times, out, ID, 'B')
 
 def main():
     pairs = pair_files()
